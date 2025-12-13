31cc324a6fcc300f9d0b143e20fd2bb8a37a38e5
yayevy <yayeverest@gmail.com>
2025-12-12 22:15:52 -0500

    wip redo for pulley & thruster ponders

--- src/main/java/yay/evy/everest/vstuff/content/ponder/PhysPulleyPonder.java
+++ src/main/java/yay/evy/everest/vstuff/content/ponder/PhysPulleyPonder.java
@@ -8,92 +8,41 @@ import net.createmod.ponder.api.scene.Selection;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
+import yay.evy.everest.vstuff.index.VStuffBlocks;
 import yay.evy.everest.vstuff.index.VStuffItems;
 
 public class PhysPulleyPonder {
 
     public static void physPulley(SceneBuilder scene, SceneBuildingUtil util) {
         scene.title("phys_pulley", "The Phys Pulley");
-        scene.configureBasePlate(0, 0, 7);
-        scene.scaleSceneView(0.7f);
-        scene.setSceneOffsetY(0);
+        scene.configureBasePlate(0, 0, 5);
         scene.removeShadow();
+        scene.scaleSceneView(0.7f);
+        scene.setSceneOffsetY(-1);
 
-        ItemStack ropeItem = new ItemStack(VStuffItems.LEAD_CONSTRAINT_ITEM.get());
 
-        BlockPos baseStart = util.grid().at(0, 0, 0);
-        BlockPos baseEnd = util.grid().at(4, 0, 4);
-        BlockPos columnStart = util.grid().at(2, 1, 2);
-        BlockPos columnEnd = util.grid().at(2, 5, 2);
-        BlockPos pulleyPos = util.grid().at(2, 5, 1);
-        BlockPos ironBlockPos = util.grid().at(2, 1, 1);
-        BlockPos crankPos = util.grid().at(3, 5, 3);
-
-        Selection basePlate = util.select().fromTo(baseStart, baseEnd);
-        Selection oakColumn = util.select().fromTo(columnStart, columnEnd);
-        Selection pulleySel = util.select().position(pulleyPos);
-        Selection crankSel = util.select().position(crankPos);
-        Selection ironSel = util.select().position(ironBlockPos);
-
-        scene.world().showSection(basePlate, Direction.DOWN);
+        // whatd yal have for breakfast, cuz i had bricks
+        Selection all = util.select().fromTo(0, 0, 0, 4, 6, 4);
+        scene.world().showSection(all, Direction.DOWN);
         scene.idle(20);
 
-        scene.world().showSection(oakColumn, Direction.UP);
-        scene.idle(20);
+        BlockPos pulleyPos = util.grid().at(2, 6, 3);
 
-        scene.world().showSection(pulleySel, Direction.UP);
-        scene.world().showSection(crankSel, Direction.UP);
-        scene.idle(20);
-
-        scene.world().showSection(ironSel, Direction.UP);
-        scene.idle(20);
-
-        scene.overlay().showText(80)
-                .text("vstuff.ponder.phys_pulley.text_1")
-                .placeNearTarget();
-        scene.idle(90);
-
-        scene.overlay().showText(80)
-                .text("vstuff.ponder.phys_pulley.text_2")
-                .placeNearTarget();
-        scene.idle(90);
-
-        scene.overlay().showText(100)
-                .text("vstuff.ponder.phys_pulley.text_3")
+        scene.overlay().showText(90)
+                .text("vstuff.ponder.phys_pulley.text_rope")
                 .placeNearTarget();
 
         scene.overlay().showControls(
-                util.vector().topOf(pulleyPos),
-                Pointing.DOWN,
-                60
-        ).rightClick().whileSneaking();
+                        util.vector().topOf(pulleyPos),
+                        Pointing.DOWN,
+                        60
+                ).withItem(new ItemStack(VStuffItems.LEAD_CONSTRAINT_ITEM.get()))
+                .rightClick()
+                .whileSneaking();
 
-        scene.idle(100);
-
-        scene.overlay().showText(100)
-                .text("vstuff.ponder.phys_pulley.text_4")
-                .placeNearTarget();
-
-        scene.overlay().showControls(
-                util.vector().topOf(ironBlockPos),
-                Pointing.UP,
-                60
-        ).withItem(ropeItem).rightClick();
-
-        scene.idle(100);
-
-        scene.overlay().showLine(
-                PonderPalette.GREEN,
-                util.vector().centerOf(pulleyPos),
-                util.vector().topOf(ironBlockPos),
-                80
-        );
-
-        scene.overlay().showText(120)
-                .text("vstuff.ponder.phys_pulley.text_5")
-                .placeNearTarget();
         scene.idle(120);
-
         scene.markAsFinished();
     }
+
+
 }
--- src/main/java/yay/evy/everest/vstuff/content/ponder/RotationalThrusterPonder.java
+++ src/main/java/yay/evy/everest/vstuff/content/ponder/RotationalThrusterPonder.java
@@ -14,52 +14,38 @@ public class RotationalThrusterPonder {
 
     public static void rotationalThruster(SceneBuilder scene, SceneBuildingUtil util) {
         scene.title("rotational_thruster", "Using the Rotational Thruster");
-
         scene.configureBasePlate(0, 0, 5);
+        scene.scaleSceneView(0.7f);
+        scene.setSceneOffsetY(-1);
+        scene.removeShadow();
 
-        Selection base = util.select().fromTo(0, 0, 0, 4, 0, 4);
-        scene.world().setBlocks(base, Blocks.GRASS_BLOCK.defaultBlockState(), false);
-        scene.world().showSection(base, Direction.DOWN);
+        // shooting create with bricks
+        Selection all = util.select().fromTo(0, 0, 0, 4, 3, 4);
+        scene.world().showSection(all, Direction.DOWN);
         scene.idle(20);
 
-        BlockPos center = util.grid().at(2, 1, 2);
-        BlockPos motor = center.west();
-
-        scene.world().showSection(util.select().position(center), Direction.DOWN);
-        scene.idle(10);
+        BlockPos thrusterPos = util.grid().at(2, 1, 2);
+        BlockPos motorPos = util.grid().at(1, 1, 2);
 
         scene.overlay().showText(60)
-                .text(Component.translatable("vstuff.ponder.rotational_thruster.title").getString())
-                .pointAt(util.vector().topOf(center))
+                .text("vstuff.ponder.rotational_thruster.title")
+                .pointAt(util.vector().topOf(thrusterPos))
                 .placeNearTarget();
         scene.idle(70);
 
-        scene.world().showSection(util.select().position(motor), Direction.DOWN);
-        scene.idle(15);
-        scene.idle(15);
-
-        BlockPos particle1 = center.above();
-        BlockPos particle2 = center.above(2);
-
-        scene.world().showSection(util.select().position(particle1), Direction.DOWN);
-        scene.idle(2);
-        scene.world().hideSection(util.select().position(particle1), Direction.UP);
-        scene.world().showSection(util.select().position(particle2), Direction.DOWN);
-        scene.idle(2);
-        scene.world().hideSection(util.select().position(particle2), Direction.UP);
-
-
-
         scene.overlay().showText(70)
-                .text(Component.translatable("vstuff.ponder.rotational_thruster.how").getString())
-                .pointAt(util.vector().topOf(motor))
+                .text("vstuff.ponder.rotational_thruster.how")
+                .pointAt(util.vector().topOf(motorPos))
                 .placeNearTarget();
         scene.idle(80);
 
         scene.overlay().showText(80)
-                .text(Component.translatable("vstuff.ponder.rotational_thruster.effect").getString())
-                .pointAt(util.vector().blockSurface(center, Direction.NORTH))
+                .text("vstuff.ponder.rotational_thruster.effect")
+                .pointAt(util.vector().blockSurface(thrusterPos, Direction.NORTH))
                 .placeNearTarget();
         scene.idle(90);
+
+        scene.markAsFinished();
     }
+
 }
