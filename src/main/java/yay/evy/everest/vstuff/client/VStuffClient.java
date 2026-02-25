de0ac2565d9939364022e4295e8727f6405748ae
yayevy <yayeverest@gmail.com>
2026-02-24 21:32:49 -0500

    fix rope thrower crashing, why is this even an issue?

--- src/main/java/yay/evy/everest/vstuff/client/VStuffClient.java
+++ src/main/java/yay/evy/everest/vstuff/client/VStuffClient.java
@@ -1,12 +1,23 @@
 package yay.evy.everest.vstuff.client;
 
 import net.createmod.ponder.foundation.PonderIndex;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+import yay.evy.everest.vstuff.index.VStuffEntities;
 import yay.evy.everest.vstuff.infrastructure.ponder.VStuffPonders;
 
 public class VStuffClient {
+
     public static void initialize(final FMLClientSetupEvent event) {
+
+        event.enqueueWork(() -> {
+            EntityRenderers.register(
+                    VStuffEntities.ROPE_THROWER.get(),
+                    ThrownItemRenderer::new
+            );
+        });
+
         PonderIndex.addPlugin(new VStuffPonders());
     }
-
 }
