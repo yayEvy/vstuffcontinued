858199db0deb5d84dd75a28d1e397b2f67fed0ec
yayevy <yayeverest@gmail.com>
2026-05-05 21:34:31 -0400

    Ropes will now conserve themselves when they are on a block that gets shipified!

--- src/main/java/yay/evy/everest/vstuff/content/ropes/RopeFactory.java
+++ src/main/java/yay/evy/everest/vstuff/content/ropes/RopeFactory.java
@@ -101,6 +101,36 @@ public class  RopeFactory {
         return rope;
     }
 
+    public static ReworkedRope reCreateNewRope(ServerLevel level, Long ship0, Long ship1, BlockPos blockPos0, BlockPos blockPos1, ResourceLocation style, Entity player) {
+        Pair<RopePosData, RopePosData> posDataPair = RopePosData.create(level, ship0, ship1, blockPos0, blockPos1);
+        RopePosData posData0 = posDataPair.component1();
+        RopePosData posData1 = posDataPair.component2();
+
+        float length = (float) posData0.getWorldPos(level).distance(posData1.getWorldPos(level)) + 0.5f;
+
+        float mass0 = ShipUtils.getMassForShip(level, ship0);
+        float mass1 = ShipUtils.getMassForShip(level, ship1);
+        // double compliance = 1e-12 / Math.max(Math.min(mass0, mass1), 100.0) * (posData0.isWorld() || posData1.isWorld() ? 0.05 : 1); maybe not needed idk
+        float maxForce = 5e13f * Math.min(Math.max(mass0, mass1) / Math.min(mass0, mass1), 20.0f) * (posData0.isWorld() || posData1.isWorld() ? 10f : 1f);
+
+        ReworkedRope rope = new ReworkedRope(posData0, posData1, JointValues.withDefault(maxForce, maxForce, length), style);
+
+        if (!rope.hasJoint) {
+            RopeManager.get(level).addRope(rope);
+            //System.out.println("attach actors");
+            rope.attachActors(level);
+
+            if (player instanceof ServerPlayer serverPlayer) {
+                RopeManager.syncAllRopesToPlayer(serverPlayer);
+            }
+        }
+        else
+            GTPAUtils.reAddRopeJoint(level, rope, player);
+
+        return rope;
+    }
+
+
     public static void removeRope(ServerLevel serverLevel, Integer ropeId) {
         RopeManager manager = RopeManager.get(serverLevel);
         ReworkedRope removed = manager.getRope(ropeId);
