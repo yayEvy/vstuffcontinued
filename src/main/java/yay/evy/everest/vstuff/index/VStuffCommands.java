4987bb89eb0954401bca5b67288ee93d16ff088b
TheBryBry154 <thebrybry154@gmail.com>
2026-05-17 17:33:44 -0400

    Delete Everything

--- src/main/java/yay/evy/everest/vstuff/index/VStuffCommands.java
+++ src/main/java/yay/evy/everest/vstuff/index/VStuffCommands.java
@@ -1,12 +1,15 @@
 package yay.evy.everest.vstuff.index;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
 import yay.evy.everest.vstuff.infrastructure.commands.CreateRopeCommand;
+import yay.evy.everest.vstuff.infrastructure.commands.DeleteAllRopesCommand;
+import yay.evy.everest.vstuff.infrastructure.commands.DeleteRopeCommand;
 
 public class VStuffCommands {
 
@@ -14,13 +17,19 @@ public class VStuffCommands {
 
         LiteralArgumentBuilder<CommandSourceStack> vstuffCommand = Commands.literal("vstuff");
 
-        vstuffCommand.then(Commands.literal("create-new-rope")
+        vstuffCommand.then(Commands.literal("CreateNewRope")
                         .then(Commands.argument("from", BlockPosArgument.blockPos())
                         .then(Commands.argument("to", BlockPosArgument.blockPos())
                         .then(Commands.argument("style", StringArgumentType.string())
                         .executes(CreateRopeCommand::createNewRope)))));
         /// todo create a RopeStylesArgument class for allowing this thing to make rope styles.
 
+        vstuffCommand.then(Commands.literal("DeleteAllRopes")
+                        .executes(DeleteAllRopesCommand::deleteCantaRopes));
+
+        vstuffCommand.then(Commands.literal("DeleteRope")
+                        .then(Commands.argument("RopeID", IntegerArgumentType.integer())
+                        .executes(DeleteRopeCommand::deleteRope)));
 
         policeDispatcher.register(vstuffCommand);
 
