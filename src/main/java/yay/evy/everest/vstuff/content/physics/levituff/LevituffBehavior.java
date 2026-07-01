6504d92e07bd7b8979d66e862616c72784243c47
yayevy <yayeverest@gmail.com>
2026-06-30 22:31:43 -0400

    port levituff unloading fix to 3.x

--- src/main/java/yay/evy/everest/vstuff/content/physics/levituff/LevituffBehavior.java
+++ src/main/java/yay/evy/everest/vstuff/content/physics/levituff/LevituffBehavior.java
@@ -15,7 +15,7 @@ import static org.joml.Math.lerp;
 
 public enum LevituffBehavior {
     UP_TO_Y_LEVEL((level, ship, levituffBlocks) -> {
-        final double gravity = -level.getGravity().y();
+        final double gravity = VStuffConfigs.server().gravity.get();
 
         final double strengthMult = VStuffConfigs.server().levituffStrengthMultiplier.get();
 
@@ -26,8 +26,7 @@ public enum LevituffBehavior {
             final double spring = error * strengthMult * ship.getMass() * gravity;
             final double damping = -ship.getVelocity().y() * ship.getMass() * VStuffConfigs.server().levituffForceDamping.get() / levituffBlocks.size();
 
-            final double forceY = spring + damping;
-            ship.applyModelForce(new Vector3d(0, forceY, 0), pos);
+            ship.applyModelForce(new Vector3d(0, spring + damping, 0), pos);
         });
     }),
     UNSTOPPABLE_WHIMSY((level, ship, levituffBlocks) -> {
@@ -40,12 +39,12 @@ public enum LevituffBehavior {
         final double forceDamping = VStuffConfigs.server().levituffForceDamping.get();
         final double damping = -velY * mass * forceDamping;
 
-        ship.applyInvariantForce(new Vector3d(0, (mass * liftMultiplier) + damping, 0));
+        final double damping = -ship.getVelocity().y() * mass * VStuffConfigs.server().levituffForceDamping.get();
     })
     ;
 
     public static double getLiftMultiplier(double y) {
-        if (y < 0) return lerp(1.6, 1.3, (y + 64) / 64.0);
+        if (y < 0)   return lerp(1.6, 1.3, (y + 64) / 64.0);
         if (y < 100) return lerp(1.3, 1.1, y / 100.0);
         if (y < 200) return lerp(1.1, 0.9, (y - 100) / 100.0);
         if (y < 300) return lerp(0.9, 0.5, (y - 200) / 100.0);
@@ -53,13 +52,13 @@ public enum LevituffBehavior {
     }
 
 
-    private final TriConsumer<FU, PhysShipImpl, Set<Vector3d>> onPhysTick;
+    private final TriConsumer<PhysLevel, PhysShipImpl, Set<Vector3d>> onPhysTick;
 
-    LevituffBehavior(TriConsumer<FU, PhysShipImpl, Set<Vector3d>> onPhysTick) {
+    LevituffBehavior(TriConsumer<PhysLevel, PhysShipImpl, Set<Vector3d>> onPhysTick) {
         this.onPhysTick = onPhysTick;
     }
 
     public void physTick(PhysLevel level, PhysShip ship, Set<Vector3d> levituffBlocks) {
-        this.onPhysTick.accept((FU) level, (PhysShipImpl) ship, levituffBlocks);
+        this.onPhysTick.accept(level, (PhysShipImpl) ship, levituffBlocks);
     }
 }
